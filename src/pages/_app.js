3e4bce9d9c0336b80da297b9842490b67f50a683
Victor Garza <victorgestaltung@gmail.com>
2022-09-08 02:25:36 -0500

    Google Analytics added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,20 @@
-import '../assets/styles/globals.css';
 import { Provider } from 'react-redux';
+import Script from 'next/script';
 import { wrapper, store } from '../routes/store';
 import Header from '../components/Header';
+import '../assets/styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-582Y4FQZNX"></Script>
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-582Y4FQZNX');`
+        }
+      </Script>
       <Header />
       <Component {...pageProps} />
     </Provider>
